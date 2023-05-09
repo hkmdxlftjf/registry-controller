838287cdef0ab290a3108b68ba8dcb4131afb38a
zhangshuo <zhang.shuo5@neolink.com>
2023-05-09 21:35:25 +0800

    add some code

--- api/dubbo/v1/dubbo_types.go
+++ api/dubbo/v1/dubbo_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	"istio.io/api/networking/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -27,19 +28,47 @@ import (
 type DubboSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	RouteConfig []RouteConfig `json:"route_config,omitempty"`
+}
 
-	// Foo is an example field of Dubbo. Edit dubbo_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+type Routes struct {
+	Method map[string]v1beta1.StringMatch `json:"method,omitempty"`
+	Route  Route                          `json:"route"`
+}
+
+type Route struct {
+	Cluster string `json:"cluster,omitempty"`
+}
+
+type RouteConfig struct {
+	Interface string   `json:"interface,omitempty"`
+	Name      string   `json:"name,omitempty"`
+	Routes    []Routes `json:"routes"`
 }
 
 // DubboStatus defines the observed state of Dubbo
 type DubboStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Status []Status `json:"status,omitempty"`
+}
+
+type Status struct {
+	Interface string `json:"interface,omitempty"`
+	Method    string `json:"method"`
+}
+
+type Destination struct {
+	Subset string `json:"subset,omitempty"`
+	Weight string `json:"weight"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+// +kubebuilder:resource:path=mykinds,scope=Namespaced
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="Spec",type="string",JSONPath=".spec.fieldName"
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase"
 
 // Dubbo is the Schema for the dubboes API
 type Dubbo struct {
